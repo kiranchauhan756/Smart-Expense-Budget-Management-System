c9c108c28fbae2dad576b53c27d650879fd21ae7
Kiran Chauhan <kc7563330@gmail.com>
2025-11-07 16:53:51 +0530

    Role based dasboard redirection

--- budget-management-system/src/main/java/com/smart_expense/budget_management_system/configuration/SecurityConfiguration.java
+++ budget-management-system/src/main/java/com/smart_expense/budget_management_system/configuration/SecurityConfiguration.java
@@ -21,8 +21,20 @@ public class SecurityConfiguration {
                         .anyRequest().authenticated())
                 .formLogin(form->
                         form.loginPage("/login")
-                                .loginProcessingUrl("/authenticateTheUser").defaultSuccessUrl("/dashboard/user").permitAll())
-                .logout(LogoutConfigurer::permitAll);
+                                .loginProcessingUrl("/authenticateTheUser").permitAll()
+                                .successHandler(((request, response, authentication) ->
+                                {
+                                    var authorities=authentication.getAuthorities();
+                                    String redirectUrl="/dashboard/user";
+                                    if(authorities.stream().anyMatch(a->a.getAuthority().equals("ROLE_ADMIN"))){
+                                        redirectUrl="/dashboard/admin";
+                                    }
+                                    else if(authorities.stream().anyMatch(a->a.getAuthority().equals("ROLE_USER"))){
+                                        redirectUrl="/dashboard/user";
+                                    }
+                                    response.sendRedirect(redirectUrl);
+                                }))
+                ).logout(LogoutConfigurer::permitAll);
         return httpSecurity.build();
     }
 }
